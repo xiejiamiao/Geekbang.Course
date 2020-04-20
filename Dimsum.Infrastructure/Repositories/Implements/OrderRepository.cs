5e8fac8f885b7e36505efb26a27f24b477bc84bb
Jiamiao.x <xiejiamiao0326@hotmail.com>
2020-04-20 18:28:57 +0800

    nothine

---
+++ Dimsum.Infrastructure/Repositories/Implements/OrderRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Dimsum.Domain.OrderAggregate;
+using Dimsum.Infrastructure.Core;
+using Dimsum.Infrastructure.Repositories.Interfaces;
+
+namespace Dimsum.Infrastructure.Repositories.Implements
+{
+    public class OrderRepository : Repository<Order, long, DomainContext>, IOrderRepository
+    {
+        public OrderRepository(DomainContext context) : base(context)
+        {
+        }
+    }
+}
---
+++ Dimsum.Infrastructure/Repositories/Interfaces/IOrderRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Dimsum.Domain.OrderAggregate;
+using Dimsum.Infrastructure.Core;
+
+namespace Dimsum.Infrastructure.Repositories.Interfaces
+{
+    public interface IOrderRepository : IRepository<Order, long>
+    {
+    }
+}
